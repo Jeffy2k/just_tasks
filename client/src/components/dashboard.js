a83ba16448fb74727cd8e1352acff2ebb0e52b3f
Jeff <jeffgichuki68@gmail.com>
2023-03-26 00:59:18 +0300

    frontend:created basic side nav structure

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -5,7 +5,11 @@ import '../styles/dashboard.css'
 function Dashboard() {
     return ( 
           <div id="dashboard-container">
-             <div className="col-nav"></div>
+             <div className="col-nav">
+                <div className='row-logo'></div>
+                <div className='row-boards'></div>
+                <div className='row-settings'></div>
+             </div>
              <div className="col-body">
                 <div className='row-nav'></div>
                 <div className='row-body'></div>
